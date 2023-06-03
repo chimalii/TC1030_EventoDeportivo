f26db7eb0a69eb290f7cc98fa328c2c4467ba8e4
Gaby Nava <111267238+chimalii@users.noreply.github.com>
2023-06-03 01:04:55 -0600

    Uso de polimorfismo en nueva clase
    Usando arreglo de apuntadores, en la nueva clase participante, se va agregando la información de las diferentes modalidades, mediante la creación de objetos de estas.
    Con las instrucciones en este main se pueden introducir datos del participante y las modalidades a inscribir, devolviendo los datos introducidos en un formato más claro, además del costo total

--- main.cpp
+++ main.cpp
@@ -1,23 +1,16 @@
 # include "modalidad.h"
+# include "participante.h"
 # include <iostream>
 
 int main(){
-    
-    Correr mod1(4.5, 1, "Canicross");
-    mod1.calc_precio(200.0);
 
-    Bicicleta mod2(4.5, 1, "Bikejoring");
-    mod2.calc_precio(300.0);
-    mod2.calc_renta();
+    Participante inscripcion;
+    inscripcion.get_nombre();
+    inscripcion.get_edad();
+    inscripcion.get_genero();
+    inscripcion.get_cant_mod();
+    inscripcion.calc_precio_total();
+    inscripcion.datos_persona();
 
-    Scooter mod3(4.5, 2, "Scooter");
-    mod3.calc_precio(300.0);
-    mod3.calc_renta();
-
-    mod1.imprime_info();
-    mod2.imprime_info();
-    mod3.imprime_info();
-
-
-
+    return 0;
 }
