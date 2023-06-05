5aadd698c8ce6d26a885978604f9ba0faa1b29ee
Gaby Nava <111267238+chimalii@users.noreply.github.com>
2023-06-05 17:16:26 -0600

    Add files via upload

--- modalidad.h
+++ modalidad.h
@@ -20,23 +20,20 @@ class Modalidad{
         void set_dist(float dist) {distancia = dist;}
         void set_num_pe(int num_pe) {num_perros = num_pe;}
 
-        virtual void calc_precio(float precio_base);
+        virtual float calc_precio(float precio_base);
+        virtual float calc_renta();
         virtual void imprime_info();
 };
 
-void Modalidad::calc_precio(float precio_base){
-    string DID;
-    string ad_DID;
-    cout << "Cuenta con DID (SI/NO): "; cin >> DID;
-    if (DID == "NO"){
-        cout << "Desea adquirirlo (SI/NO): "; cin >> ad_DID;
-        if (ad_DID == "NO"){
-            precio = precio_base;
-        }
-        precio = precio_base*0.8;
-    }
-    precio = precio_base*0.8;
+float Modalidad::calc_precio(float precio_base){
+    precio = precio_base;  
+    return precio;
 }
+
+float Modalidad::calc_renta(){
+    return 0;
+}
+
 
 void Modalidad::imprime_info(){
     cout << "Distancia: " << distancia << endl;
@@ -56,7 +53,6 @@ class Correr: public Modalidad{
             tipo = t;
 		}
         
-        // void calc_precio(float precio_base);
         void imprime_info();
 };
 
@@ -65,7 +61,6 @@ void Correr::imprime_info(){
     cout << "Distancia: " << distancia << " km" << endl;
     cout << "Cant de perros para participar: " << num_perros << endl;
     cout << "Tipo: " << tipo << endl;
-    cout << "Costo: " << precio << " pesos" << endl;
 }
 
 class Bicicleta: public Modalidad{
@@ -81,7 +76,7 @@ class Bicicleta: public Modalidad{
             tipo = t;
 		}
 
-        void calc_renta();
+        float calc_renta();
         void imprime_info();
 
 };
@@ -99,27 +94,31 @@ bool Bicicleta::get_renta(){
 }
 */
 
-void Bicicleta::calc_renta(){
+float Bicicleta::calc_renta(){
+      float r_b_1 = 80.0;
+    float r_b_2 = 250.0;
+    float r_b_3 = 300.0;
     string renta;
     int opcion;
-    cout << "Deseas rentar equipo para BICICLETA (SI/NO): "; cin >> renta;
+    cout << endl << "Deseas rentar equipo para BICICLETA (SI/NO): "; cin >> renta;
     if (renta == "SI"){
         cout << endl << "*.* IMPORTANTE *.*" << endl
         << "Si el equipo sufre algun danio se cobraran extras en reparacion." << endl
-        << endl << " 1. Accesorios (lentes, casco, guantes) -------------------------> $100" << endl
-        << " 2. Solo Bicicleta ----------------------------------------------> $300" << endl
-        << " 3. Bicicleta y accesorios --------------------------------------> $350" << endl
+        << endl << " 1. Accesorios (lentes, casco, guantes) -------------------------> $" << r_b_1 << ".00" << endl
+        << " 2. Solo Bicicleta ----------------------------------------------> $" << r_b_2 << ".00" << endl
+        << " 3. Bicicleta y accesorios --------------------------------------> $" << r_b_3 << ".00" << endl
         << "Elige el equipo que deseas rentar: ";
         cin >> opcion;
         if (opcion == 1){
-            precio_renta = 100.0;
+            precio_renta = r_b_1; return precio_renta;
         }
         else if (opcion == 2){
-            precio_renta = 300.0;
+            precio_renta = r_b_2; return precio_renta;
         }
-        else {precio_renta = 350.0;}
+        else {precio_renta = r_b_3; return precio_renta;
+        }
     }
-    else {precio_renta = 0.0;}
+    else {precio_renta = 0.0; return precio_renta;}
 }
 
 void Bicicleta::imprime_info(){
@@ -130,21 +129,16 @@ void Bicicleta::imprime_info(){
     cout << "Tipo: " << tipo << endl;
     if (precio_renta != 0.0){
         cout << "Rentaste: ";
-        if (precio_renta == 100.0){
-            cout << "ACCESORIOS, que incluyen casco, lentes y guantes." << endl;
-            precio_total = precio + precio_renta;
+        if (precio_renta == 80.0){
+            cout << "ACCESORIOS, que incluyen casco, lentes y guantes.()" << endl;
         }
-        else if (precio_renta == 300.0){
+        else if (precio_renta == 270.0){
             cout << "Solo la BICICLETA." << endl;
-            precio_total = precio + precio_renta;
         }
         else {
             cout << "BICICLETA y ACCESORIOS, que incluyen casco, lentes y guantes." << endl;
-            precio_total = precio + precio_renta;
         }
-        cout << "Costo: " << precio_total << " pesos" << endl;
     }
-    else {cout << "Costo: " << precio << " pesos" << endl;}
 }
 
 class Scooter: public Modalidad{
@@ -160,31 +154,35 @@ class Scooter: public Modalidad{
             tipo = t;
 		}
 
-        void calc_renta();
+        float calc_renta();
         void imprime_info();
 }; 
 
-void Scooter::calc_renta(){
+float Scooter::calc_renta(){
+    float r_s_1 = 80.0;
+    float r_s_2 = 270.0;
+    float r_s_3 = 320.0;
     string renta;
     int opcion;
-    cout << "Deseas rentar equipo para SCOOTER (SI/NO): "; cin >> renta;
+    cout << endl << "Deseas rentar equipo para SCOOTER (SI/NO): "; cin >> renta;
     if (renta == "SI"){
         cout << endl << "*.* IMPORTANTE *.*" << endl
         << "Si el equipo sufre algun danio se cobraran extras en reparacion." << endl
-        << endl << " 1. Accesorios (lentes, casco, guantes) -------------------------> $100" << endl
-        << " 2. Solo Scooter ------------------------------------------------> $320" << endl
-        << " 3. Scooter y accesorios ----------------------------------------> $370" << endl
+        << endl << " 1. Accesorios (lentes, casco, guantes) -------------------------> $" << r_s_1 << ".00" << endl
+        << " 2. Solo Scooter ------------------------------------------------> $" << r_s_2 << ".00" << endl
+        << " 3. Scooter y accesorios ----------------------------------------> $" << r_s_3 << ".00" << endl
         << "Elige el equipo que deseas rentar: ";
         cin >> opcion;
         if (opcion == 1){
-            precio_renta = 100.0;
+            precio_renta = r_s_1; return precio_renta;
         }
         else if (opcion == 2){
-            precio_renta = 320.0;
+            precio_renta = r_s_2; return precio_renta;
         }
-        else {precio_renta = 370.0;}
+        else {precio_renta = r_s_3; return precio_renta;
+        }
     }
-    else {precio_renta = 0.0;}
+    else {precio_renta = 0.0; return precio_renta;}
 }
 
 void Scooter::imprime_info(){
@@ -195,21 +193,16 @@ void Scooter::imprime_info(){
     cout << "Tipo: " << tipo << endl;
     if (precio_renta != 0.0){
         cout << "Rentaste: ";
-        if (precio_renta == 100.0){
+        if (precio_renta == 80.0){
             cout << "ACCESORIOS, que incluyen casco, lentes y guantes." << endl;
-            precio_total = precio + precio_renta;
         }
-        else if (precio_renta == 320.0){
+        else if (precio_renta == 270.0){
             cout << "Solo el SCOOTER." << endl;
-            precio_total = precio + precio_renta;
         }
         else {
-            cout << "SCOOTER y ACCESORIOS, que incluyen casco, lentes y guantes." << endl;
-            precio_total = precio + precio_renta;
+            cout << "SCOOTER y ACCESORIOS, que incluyen casco, lentes y guantes. ($250.00)" << endl;
         }
-        cout << "Costo: " << precio_total << " pesos" << endl;
     }
-    else {cout << "Costo: " << precio << " pesos" << endl;}
 }
 
 
