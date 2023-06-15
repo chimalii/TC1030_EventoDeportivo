6e725d50d6930048ad1abd2b0b79b98b4c204c13
Gaby Nava <111267238+chimalii@users.noreply.github.com>
2023-06-14 22:42:17 -0600

    Cambios en calc_precio, desparece calc_renta
    Se simplificó el calculo del precio, juntando el calculo de renta con en del precio por modalidad, sobrescribiéndolo en aquellas que lo requerían. Para esto también se agregaron algunas variables a cada clase, así como parámetros a los constructores para que el único archivo que se deba modificar por los "organizadores" del Evento Deportivo sea el correspondiente a datos y la impresión de estos sea algo más sencilla

--- modalidad.h
+++ modalidad.h
@@ -2,208 +2,189 @@
 #define MODALIDAD_H
 
 #include <iostream>
+#include <sstream>
+#include <string>
 using namespace std;
 
-class Modalidad{
-    protected:
-        float distancia;
-        int num_perros;
-        float precio;
-    
-    public:
-        Modalidad(): distancia(0), num_perros(0) {};
-        Modalidad(float dist, int num_pe):
-        distancia(dist), num_perros(num_pe) {};
+class Modalidad {
+protected:
+    string tipo;
+    float distancia, precio;
+    int num_perros;
 
-        float get_dist(){return distancia;};
-        int get_num_pe(){return num_perros;};
-        void set_dist(float dist) {distancia = dist;}
-        void set_num_pe(int num_pe) {num_perros = num_pe;}
+public:
+    // Constructor default
+    Modalidad():  tipo(""), distancia(0), num_perros(0) {}
+    Modalidad(string t, float dist, int num_pe): 
+        tipo(t), distancia(dist), num_perros(num_pe){};
 
-        virtual float calc_precio(float precio_base) = 0;
-        virtual float calc_renta() = 0;
-        virtual void imprime_info() = 0;
+    float get_dist() { return distancia; }
+    int get_num_pe() { return num_perros; }
+    void set_dist(float dist) { distancia = dist; }
+    void set_num_pe(int num_pe) { num_perros = num_pe; }
+
+    virtual float calc_precio(float) = 0;
+    virtual void imprime_info() = 0;
 };
 
-float Modalidad::calc_precio(float precio_base){
-    precio = precio_base;  
+// / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+
+class Correr: public Modalidad{
+
+    public:
+        // Constructor default
+        Correr(): Modalidad("Canicross", 0, 1) {}
+        Correr(float dist, int num_pe): Modalidad("Canicross", dist, num_pe) {};
+
+        float calc_precio(float precio_base);       
+        void imprime_info();
+};
+
+float Correr::calc_precio(float precio_base){
+    precio = precio_base;
     return precio;
 }
 
-float Modalidad::calc_renta(){
-    return 0;
-}
-
-
-void Modalidad::imprime_info(){
-    cout << "Distancia: " << distancia << endl;
-    cout << "Cantidad de perros para participar: " << num_perros << endl;
-}
-
-
-class Correr: public Modalidad{
-    private:
-        string tipo;
-
-    public:
-        Correr(): tipo(""), Modalidad(){};
-        Correr(float dist, int num_pe, string t):Modalidad(dist, num_pe){ 
-			distancia = dist;
-            num_perros = num_pe;
-            tipo = t;
-		}
-        
-        void imprime_info();
-};
-
 void Correr::imprime_info(){
-    cout << endl << "----------- DATOS " << tipo << " -----------" << endl;
-    cout << "Distancia: " << distancia << " km" << endl;
-    cout << "Cant de perros para participar: " << num_perros << endl;
-    cout << "Tipo: " << tipo << endl;
+    cout << "\n----------- DATOS CANICROSS -----------"
+    << "\nDistancia: " << distancia << " km"
+    << "\nCant de perros para participar: " << num_perros
+    << "\nCosto: $" << precio << " pesos\n";
 }
+
+// / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
 
 class Bicicleta: public Modalidad{
     private:
-        string tipo;
-        float precio_renta;
+        float renta_1, renta_2, renta_3;
+        float precio_renta = 0.0;
+        string eq_1 = "ACCESORIOS (lentes, casco, guantes)";
+        string eq_2 = "Solo BICICLETA";
+        string eq_3 = "BICICLETA y ACCESORIOS ";
 
     public:
-        Bicicleta(): tipo(""), Modalidad(){};
-        Bicicleta(float dist, int num_pe, string t):Modalidad(dist, num_pe){ 
-			distancia = dist;
-            num_perros = num_pe;
-            tipo = t;
-		}
+        Bicicleta(): Modalidad("Bikejoring", 0.0, 1) {}
+        Bicicleta(float dist, int num_pe, float r_1, float r_2, float r_3): 
+            Modalidad("Bikejoring", dist, num_pe), renta_1(r_1), renta_2(r_2), renta_3(r_3) {}; 
 
-        float calc_renta();
+        float calc_precio(float precio_base);
         void imprime_info();
-
 };
 
-/*
-bool Bicicleta::get_renta(){
-    string renta;
-    cout << "Deseas rentar equipo (SI/NO): "; cin >> renta;
-    if (renta == "SI"){
-        renta_equipo = true;
-        return renta_equipo;
-    }
-    renta_equipo = false;
-    return renta_equipo;
-}
-*/
-
-float Bicicleta::calc_renta(){
-      float r_b_1 = 80.0;
-    float r_b_2 = 250.0;
-    float r_b_3 = 300.0;
+float Bicicleta::calc_precio(float precio_base){
     string renta;
     int opcion;
-    cout << endl << "Deseas rentar equipo para BICICLETA (SI/NO): "; cin >> renta;
+    cout << "\nDeseas rentar equipo para BIKEJORING (SI/NO): "; cin >> renta;
     if (renta == "SI"){
-        cout << endl << "*.* IMPORTANTE *.*" << endl
-        << "Si el equipo sufre algun danio se cobraran extras en reparacion." << endl
-        << endl << " 1. Accesorios (lentes, casco, guantes) -------------------------> $" << r_b_1 << ".00" << endl
-        << " 2. Solo Bicicleta ----------------------------------------------> $" << r_b_2 << ".00" << endl
-        << " 3. Bicicleta y accesorios --------------------------------------> $" << r_b_3 << ".00" << endl
-        << "Elige el equipo que deseas rentar: ";
+        cout << "\n   *.* IMPORTANTE *.*\nSi el equipo sufre algun danio se cobraran extras en reparacion.\n"
+        << " 1. " << eq_1 << "-------------------------> $" << renta_1 << ".00\n"
+        << " 2. " << eq_2 << "----------------------------------------------> $" << renta_2 << ".00\n"
+        << " 3. " << eq_3 << " --------------------------------------> $" << renta_3 << ".00\n"
+        << "\nElige el equipo que deseas rentar: ";
+        
         cin >> opcion;
         if (opcion == 1){
-            precio_renta = r_b_1; return precio_renta;
+            precio_renta = renta_1;
+            return precio = precio_base + precio_renta;
         }
         else if (opcion == 2){
-            precio_renta = r_b_2; return precio_renta;
+            precio_renta = renta_2;
+            return precio = precio_base + precio_renta; 
         }
-        else {precio_renta = r_b_3; return precio_renta;
+        else if (opcion == 3){
+            precio_renta = renta_3;
+            return precio = precio_base + precio_renta;
         }
     }
-    else {precio_renta = 0.0; return precio_renta;}
+    return precio = precio_base;
 }
 
 void Bicicleta::imprime_info(){
-    float precio_total;
-    cout << endl << "----------- DATOS " << tipo << " -----------" << endl;
-    cout << "Distancia: " << distancia << " km" << endl;
-    cout << "Cant de perros para participar: " << num_perros << endl;
-    cout << "Tipo: " << tipo << endl;
+    cout << "\n----------- DATOS BIKEJORING -----------" 
+    << "\nDistancia: " << distancia << " km"
+    << "\nCant de perros para participar: " << num_perros
+    << "\nCosto: $" << precio << " pesos\n";
     if (precio_renta != 0.0){
         cout << "Rentaste: ";
-        if (precio_renta == 80.0){
-            cout << "ACCESORIOS, que incluyen casco, lentes y guantes.()" << endl;
+        if (precio_renta == renta_1){
+            cout << eq_1;
         }
-        else if (precio_renta == 270.0){
-            cout << "Solo la BICICLETA." << endl;
+        else if (precio_renta == renta_2){
+            cout << eq_2;
         }
-        else {
-            cout << "BICICLETA y ACCESORIOS, que incluyen casco, lentes y guantes." << endl;
+        else if (precio_renta == renta_3){ 
+            cout << eq_3;
         }
+        cout << " /// Por $" << precio_renta << " pesos \n";
     }
 }
+
+// / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
 
 class Scooter: public Modalidad{
     public:
-        string tipo;
-        float precio_renta;
+        float renta_1, renta_2, renta_3;
+        float precio_renta = 0.0;
+        string eq_1 = "ACCESORIOS (lentes, casco, guantes)";
+        string eq_2 = "Solo SCOOTER";
+        string eq_3 = "SCOOTER y ACCESORIOS ";
     
     public:
-        Scooter(): tipo(""), Modalidad(){};
-        Scooter(float dist, int num_pe, string t):Modalidad(dist, num_pe){ 
-			distancia = dist;
-            num_perros = num_pe;
-            tipo = t;
-		}
+        Scooter(): Modalidad("Scooter", 0.0, 1){};
+        Scooter(float dist, int num_pe, float r_1, float r_2, float r_3): 
+            Modalidad("Scooter", dist, num_pe), renta_1(r_1), renta_2(r_2), renta_3(r_3) {};
 
-        float calc_renta();
+        float calc_precio(float precio_base);
         void imprime_info();
-}; 
+};
 
-float Scooter::calc_renta(){
-    float r_s_1 = 80.0;
-    float r_s_2 = 270.0;
-    float r_s_3 = 320.0;
+float Scooter::calc_precio(float precio_base){
     string renta;
     int opcion;
-    cout << endl << "Deseas rentar equipo para SCOOTER (SI/NO): "; cin >> renta;
+    cout << "\nDeseas rentar equipo para SCOOTER (SI/NO): "; cin >> renta;
     if (renta == "SI"){
-        cout << endl << "*.* IMPORTANTE *.*" << endl
-        << "Si el equipo sufre algun danio se cobraran extras en reparacion." << endl
-        << endl << " 1. Accesorios (lentes, casco, guantes) -------------------------> $" << r_s_1 << ".00" << endl
-        << " 2. Solo Scooter ------------------------------------------------> $" << r_s_2 << ".00" << endl
-        << " 3. Scooter y accesorios ----------------------------------------> $" << r_s_3 << ".00" << endl
-        << "Elige el equipo que deseas rentar: ";
+        cout << "\n*.* IMPORTANTE *.*\n Si el equipo sufre algun danio se cobraran extras en reparacion."
+        << "\n1. " << eq_1 << " -------------------------> $" << renta_1 << ".00"
+        << "\n2. " << eq_2 << " ------------------------------------------------> $" << renta_2 << ".00"
+        << "\n3. " << eq_3 << " ----------------------------------------> $" << renta_3 << ".00\n"
+        << "\nElige el equipo que deseas rentar: ";
+        
+        // Se introduce el numero respecto a lo que se quiere rentar
         cin >> opcion;
         if (opcion == 1){
-            precio_renta = r_s_1; return precio_renta;
+            precio_renta = renta_1;
+            return precio = precio_base + precio_renta;
         }
         else if (opcion == 2){
-            precio_renta = r_s_2; return precio_renta;
+            precio_renta = renta_2;
+            return precio = precio_base + precio_renta; 
         }
-        else {precio_renta = r_s_3; return precio_renta;
+        else if (opcion == 3){
+            precio_renta = renta_3;
+            return precio = precio_base + precio_renta;
         }
     }
-    else {precio_renta = 0.0; return precio_renta;}
+    return precio = precio_base;
 }
 
 void Scooter::imprime_info(){
-    float precio_total;
-    cout << endl << "----------- DATOS " << tipo << " -----------" << endl;
-    cout << "Distancia: " << distancia << " km" << endl;
-    cout << "Cant de perros para participar: " << num_perros << endl;
-    cout << "Tipo: " << tipo << endl;
+    cout << "\n----------- DATOS SCOOTER -----------" 
+    << "\nDistancia: " << distancia << " km"
+    << "\nCant de perros para participar: " << num_perros
+    << "\nCosto: $" << precio << " pesos\n";
     if (precio_renta != 0.0){
         cout << "Rentaste: ";
-        if (precio_renta == 80.0){
-            cout << "ACCESORIOS, que incluyen casco, lentes y guantes." << endl;
+        if (precio_renta == renta_1){
+            cout << eq_1;
         }
-        else if (precio_renta == 270.0){
-            cout << "Solo el SCOOTER." << endl;
+        else if (precio_renta == renta_2){
+            cout << eq_2;
         }
-        else {
-            cout << "SCOOTER y ACCESORIOS, que incluyen casco, lentes y guantes. ($250.00)" << endl;
+        else if (precio_renta == renta_3){ 
+            cout << eq_3;
         }
+        cout << " /// Por $" << precio_renta << " pesos \n";
     }
 }
-
 
 #endif
