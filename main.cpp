6ac919de0e80c3077ff1b64145a64fb1b7e6e434
Gaby Nava <111267238+chimalii@users.noreply.github.com>
2023-06-16 04:48:53 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <string>
-#include <cstdio> //para guardar info que se imprime en consola en un archivo tipo txt
+/*
+ * Proyecto Evento Deportivo
+ * Gabriela Chimali Nava Ramirez
+ * A01710530
+ * 16/06/2023
+ *
+ * Este programa permite llenar ordenadamente una inscripcion 
+ * a una carrera con diferentes modalidades, introducir datos
+ * del participante y las mascotas con las que competira.
+ *
+ */
+
+//Bibliotecas
+#include <iostream> //permite realizar operaciones de entrada y salida en consola.
+#include <cstdlib> //para generar de numeros aleatorios y manipular cademas de caracteres.
+#include <ctime> //para trabajar con hora, apoya a generacion de numero aleatorio.
+#include <string> //para usar getline y leer una linea completa de entrada.
+#include <cstdio> //para guardar info que se imprime en consola en un archivo tipo txt.
 
 #include "inscripcion.h"
 
@@ -23,6 +36,7 @@ int main(){
 
     insc.imprime_todo();
 
+    //Escribir en el archivo inscripcion_lleva.txt la informacion introducida por el usuario
     FILE* archivo = freopen("inscripcion_llena.txt", "w", stdout);
     insc.imprime_todo();
     fclose(archivo);
